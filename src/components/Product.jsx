77347a589d2dcb4600893409b3f6aaba5c9f1813
ahmed abdallah <103667358+ahmed441abdallah@users.noreply.github.com>
2024-02-06 23:28:44 +0200

    product modal

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-import { IoAddCircleSharp } from "react-icons/io5";
+import React, { useState } from "react";
+import Modal from "react-modal";
+import { RiCloseCircleFill } from "react-icons/ri";
+import { RiShoppingBasketLine } from "react-icons/ri";
 
 const Product = ({ product }) => {
+  const [productDetails, setProductDetails] = useState("");
+  const openModal = (product) => {
+    setProductDetails(product);
+  };
+  const closeModal = () => {
+    setProductDetails(false);
+  };
   return (
     <div className=" text-center sm:text-left mb-4 rounded-lg shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
       <div
@@ -14,7 +23,7 @@ const Product = ({ product }) => {
           src="https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg"
           alt="product"
         />
-        <a href="#!">
+        <a onClick={() => openModal(product)}>
           <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
         </a>
       </div>
@@ -22,17 +31,39 @@ const Product = ({ product }) => {
         <h5 className="mb-2 text-xl font-medium leading-tight">
           {product.title}
         </h5>
-        <p className="mb-4 text-base  ">{product.description}</p>
+        <p className="mb-4 text-base">{product.description}</p>
         <button
           type="button"
           className=" flex mx-auto sm:mx-0 items-center gap-2 rounded bg-black text-white  px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal  shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
           data-te-ripple-init
           data-te-ripple-color="light"
         >
+          <RiShoppingBasketLine className=" text-2xl"></RiShoppingBasketLine>
           Add to cart
-          <IoAddCircleSharp className=" text-2xl"></IoAddCircleSharp>
         </button>
       </div>
+      {
+        <Modal isOpen={productDetails}>
+          <div className="flex">
+            <div className=" w-1/2 m-auto text-center">
+              <img
+                src="https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg"
+                alt="products"
+                className=" rounded-xl"
+              ></img>
+              <h1 className=" text-2xl font-bold mt-4">
+                {productDetails.title}
+              </h1>
+              <p> {productDetails.description}</p>
+              <h3 className=" text-red-600"> {productDetails.price} $</h3>
+            </div>
+            <RiCloseCircleFill
+              className=" text-2xl cursor-pointer"
+              onClick={closeModal}
+            ></RiCloseCircleFill>
+          </div>
+        </Modal>
+      }
     </div>
   );
 };
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Filter from "./Filter";
 import ProductList from "./ProductList";
+import productsData from "../data.json";
 
 const Shop = () => {
+  const [products, setProducts] = useState(productsData);
+
   return (
     <div className="container mx-auto px-8 ">
       <h1 className="text-center text-5xl my-8">Browse Products</h1>
       <div>
         <Filter></Filter>
-        <ProductList></ProductList>
+        <ProductList products={products}></ProductList>
       </div>
     </div>
   );
