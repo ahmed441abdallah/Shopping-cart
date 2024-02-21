83f0b21447f06c6dac6f31ee0a6ecea9066b7ef9
ahmed abdallah <103667358+ahmed441abdallah@users.noreply.github.com>
2024-02-21 21:06:42 +0200

    add paganation

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,18 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 import Product from "./Product";
 
 const ProductList = ({ products, addToCart, addWishList }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(8);
+  const totalPages = Math.ceil(products.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentItems = products.slice(startIndex, endIndex);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const renderPagination = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(
+        <li key={i}>
+          <a
+            href="#/"
+            onClick={() => handlePageChange(i)}
+            className={`flex items-center justify-center px-3 h-8 leading-tight ${
+              currentPage === i
+                ? "text-blue-600 bg-blue-50 border border-blue-300"
+                : "text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+            }`}
+          >
+            {i}
+          </a>
+        </li>
+      );
+    }
+    return pageNumbers;
+  };
+
   return (
-    <div className="products grid gap-6 lg:grid-cols-4 md:grid-cols-3">
-      {products.map((product) => (
-        <Product
-          key={product.id}
-          product={product}
-          addToCart={addToCart}
-          addWishList={addWishList}
-        ></Product>
-      ))}
-    </div>
+    <>
+      <div className="products grid gap-6 lg:grid-cols-4 md:grid-cols-3">
+        {currentItems.map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            addToCart={addToCart}
+            addWishList={addWishList}
+          ></Product>
+        ))}
+      </div>
+      <nav aria-label="Page navigation example">
+        <ul className="flex items-center justify-center -space-x-px h-8 text-sm">
+          <li>
+            <a
+              href="#/"
+              onClick={() => handlePageChange(currentPage - 1)}
+              className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700"
+            >
+              <span className="sr-only">Previous</span>
+              <svg
+                className="w-2.5 h-2.5 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 1 1 5l4 4"
+                />
+              </svg>
+            </a>
+          </li>
+          {renderPagination()}
+          <li>
+            <a
+              href="#/"
+              onClick={() => handlePageChange(currentPage + 1)}
+              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700"
+            >
+              <span className="sr-only">Next</span>
+              <svg
+                className="w-2.5 h-2.5 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
--- src/components/Wishlist .jsx
+++ src/components/Wishlist .jsx
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoMdCloseCircleOutline } from "react-icons/io";
 import { FaCartShopping } from "react-icons/fa6";
 import Empty from "../imgs/Empty-amico.png";
 
 const Wishlist = ({ wishListItem, addToCart, removeFromWhishList }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(4);
+  const totalPages = Math.ceil(wishListItem.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentItems = wishListItem.slice(startIndex, endIndex);
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const renderPagination = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(
+        <li key={i}>
+          <a
+            href="#/"
+            onClick={() => handlePageChange(i)}
+            className={`flex items-center justify-center px-3 h-8 leading-tight ${
+              currentPage === i
+                ? "text-blue-600 bg-blue-50 border border-blue-300"
+                : "text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+            }`}
+          >
+            {i}
+          </a>
+        </li>
+      );
+    }
+    return pageNumbers;
+  };
   return (
     <div className="container mx-auto p-6">
       <h1 className="text-2xl mb-4">Wishlist 😊</h1>
       {wishListItem.length >= 1 ? (
-        wishListItem.map((el) => (
+        currentItems.map((el) => (
           <div
             className="wishlist flex justify-between items-center mb-4 shadow-lg p-2"
             key={el.id}
@@ -48,6 +79,61 @@ const Wishlist = ({ wishListItem, addToCart, removeFromWhishList }) => {
           ></img>
           <p className=" text-2xl text-center">Wishlist is empty 😶</p>
         </div>
+      )}
+      {wishListItem.length >= 1 && (
+        <nav aria-label="Page navigation example">
+          <ul className="flex items-center justify-center -space-x-px h-8 text-sm">
+            <li>
+              <a
+                href="#/"
+                onClick={() => handlePageChange(currentPage - 1)}
+                className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700"
+              >
+                <span className="sr-only">Previous</span>
+                <svg
+                  className="w-2.5 h-2.5 rtl:rotate-180"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 1 1 5l4 4"
+                  />
+                </svg>
+              </a>
+            </li>
+            {renderPagination()}
+            <li>
+              <a
+                href="#/"
+                onClick={() => handlePageChange(currentPage + 1)}
+                className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700"
+              >
+                <span className="sr-only">Next</span>
+                <svg
+                  className="w-2.5 h-2.5 rtl:rotate-180"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+              </a>
+            </li>
+          </ul>
+        </nav>
       )}
     </div>
   );
