5804f960ec8d7331884f6cdc449a00883bdb254a
ahmed abdallah <103667358+ahmed441abdallah@users.noreply.github.com>
2024-03-04 09:47:57 +0200

    2thd version of code

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,13 +16,10 @@ const Login = ({ setIsLogin }) => {
   const handleLoginSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(
-        "http://localhost:3004/products/login",
-        {
-          email,
-          password,
-        }
-      );
+      const response = await axios.post("http://localhost:3004/login", {
+        email,
+        password,
+      });
       const { token } = response.data;
       localStorage.setItem("token", token);
       setIsLogin(true); // Store the token in localStorage
@@ -105,7 +102,7 @@ const Login = ({ setIsLogin }) => {
             </div>
             <button
               type="submit"
-              className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Login
             </button>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -12,7 +12,6 @@ const Register = () => {
   const [error, setError] = useState("");
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     try {
       const response = await axios.post("http://localhost:3004/signup", {
         email,
@@ -25,6 +24,7 @@ const Register = () => {
     } catch (error) {
       if (error.response) {
         setError(error.response.data.message);
+        console.log(error);
         // Handle specific error messages from the backend
       } else {
         setError("An unexpected error occurred");
@@ -99,7 +99,7 @@ const Register = () => {
             </div>
             <button
               type="submit"
-              className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Signup
             </button>
